a175d4b7dca152b2a65a204cce968bdc8a899cd2
xatomen <jorgito899@gmail.com>
2022-11-19 22:05:52 -0300

    Se agrega la cabecera y el pie

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SIA GC STOCK EMBG</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php include("template/cabecera.php")?>
+
+<div class="jumbotron">
+    <h1 class="display-3">Jumbo heading</h1>
+    <p class="lead">Jumbo helper text</p>
+    <hr class="my-2">
+    <p>More info</p>
+    <p class="lead">
+        <a class="btn btn-primary btn-lg" href="Jumbo action link" role="button">Jumbo action name</a>
+    </p>
+</div>
+
+<?php include("template/pie.php")?>
