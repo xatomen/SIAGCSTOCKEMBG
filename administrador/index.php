3ab65040f8d467a4de14768c6df0beb9b0a51f5a
xatomen <jorgito899@gmail.com>
2022-11-19 22:07:37 -0300

    Se ingresa carpeta administrador

---
+++ administrador/index.php
@@ -0,0 +1,50 @@
+<?php
+    if($_POST){
+        header('Location:inicio.php');
+    }
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="container">
+        <div class="row">
+
+            <div class="col-md-4"></div>
+            
+            <div class="col-md-4">
+                <br/><br/><br/>
+                <div class="card">
+                    <div class="card-header">
+                        Login
+                    </div>
+                    <div class="card-body">
+                        <form method="POST">
+                            <div class = "form-group">
+                                <label for="exampleInputEmail1">Usuario</label>
+                                <input type="text" class="form-control" name="usuario" placeholder="12345678-9">
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleInputPassword1">Contraseña:</label>
+                                <input type="password" class="form-control" name="contraseña" placeholder="Contraseña">
+                            </div>
+                            <button type="submit" class="btn btn-primary">Ingresar al sistema</button>
+                        </form>
+                        
+                        
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+  </body>
+</html>
